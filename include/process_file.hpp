914547a464175e5788abcbe357d2c5ab53b072b5
Camden Blake <blakec4@rpi.edu>
2024-03-22 16:14:53 -0400

    Initial project setup

---
+++ include/process_file.hpp
@@ -0,0 +1,9 @@
+#ifndef MAVOR_PROCESS_FILE_H 
+#define MAVOR_PROCESS_FILE_H 
+
+#include <iostream>
+
+// Function calls
+void process_file(const std::string file_path);
+
+#endif
---
+++ src/process_file.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void process_file(const std::string file_path){
+    std::cout << file_path << '\n';
+}

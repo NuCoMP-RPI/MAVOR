d6c9d4c1807070ffa4d41b12af06457a979ccd05
Camden Blake <blakec4@rpi.edu>
2024-03-26 15:37:39 -0400

    Change TslData Struct to Class

--- include/file_read.hpp
+++ include/file_read.hpp
@@ -5,24 +5,25 @@
 #include <vector>
 
 // Data Structures
-typedef struct {
-    int lat;
-    int lasym;
-    int lln;
-    int za;
-    int mat;
-    float temp;
-    float t_eff;
-    float temp_ratio;
-    float a0;
-    float e_max;
-    float m0;
-    float free_xs;
-    float bound_xs;
-    std::vector<float> alphas;
-    std::vector<float> betas;
-    std::vector<float> tsl_vals;
-} TslData;
+class TslData {
+    public:
+        int lat;
+        int lasym;
+        int lln;
+        int za;
+        int mat;
+        float temp;
+        float t_eff;
+        float temp_ratio;
+        float a0;
+        float e_max;
+        float m0;
+        float free_xs;
+        float bound_xs;
+        std::vector<float> alphas;
+        std::vector<float> betas;
+        std::vector<float> tsl_vals;
+};
 
 // Function calls
 void read_file(const std::string& file_path, TslData& data);

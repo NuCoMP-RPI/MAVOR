c997c3d72d6c5ddf4908aef6319d887f9d3885ae
Camden Blake <blakec4@rpi.edu>
2024-04-19 09:58:02 -0400

    Add sigmoid space, pdf_to_cdf, and fit_cdf

--- include/utilities.hpp
+++ include/utilities.hpp
@@ -5,5 +5,8 @@
 
 std::vector<double> linspace(double const& start, double const& stop, int const& num_points);
 std::vector<double> logspace(double const& start, double const& stop, int const& num_points);
+std::vector<double> sigmoid_space(double const & low, double const & high, int const & num, double const & extent = 5);
+std::vector<double> pdf_to_cdf(std::vector<double> const & x, std::vector<double> const & y, int const & integration_scheme = 2);
+std::vector<double> fit_cdf(std::vector<double> const & x, std::vector<double> const & old_y, std::vector<double> const & new_y, int const & interpolation_scheme = 2);
 
 #endif
--- src/utilities.cpp
+++ src/utilities.cpp
@@ -1,5 +1,9 @@
 #include <vector>
-#include <cmath>
+#include <math.h>
+#include <iostream>
+
+#include "integration.hpp"
+#include "interpolation.hpp"
 
 std::vector<double> linspace(double const& start, double const& stop, int const& num_points){
     std::vector<double> result(num_points);
@@ -20,4 +24,50 @@ std::vector<double> logspace(double const& start, double const& stop, int const&
         result[i] = std::pow(10, exponent);
     }
     return result;
+}
+
+std::vector<double> sigmoid_space(double const & low, double const & high, int const & num, double const & extent = 5){
+    if (num<2){
+        throw std::domain_error("'num' must be greater than 1.");        
+    }
+    else if (num==2)
+    {
+        return std::vector<double> {low, high};
+    }
+    else
+    {
+        std::vector<double> xs = linspace(-extent, extent, num);
+        std::vector<double> ys;
+        ys.reserve(xs.size());
+        for (auto x:xs){
+            ys.push_back((1/(1 + std::exp(-x)))*(high-low) + low);
+        }
+        ys.front() = low;
+        ys.back() = high;
+        return ys;
+    }
+}
+
+std::vector<double> pdf_to_cdf(std::vector<double> const & x, std::vector<double> const & y, int const & integration_scheme){
+    std::vector<double> cdf;
+    cdf.resize(x.size());
+    cdf[0] = 0;
+    for (int i=1; i<x.size(); i++){
+        cdf[i] = cdf[i-1] + ENDF_integrate(x[i-1], x[i], y[i-1], y[i], integration_scheme);
+    }
+    double factor = 1/cdf.back();
+    for (int i=1; i<cdf.size(); i++){
+        cdf[i] *= factor;
+    }
+    return cdf;
+}
+
+std::vector<double> fit_cdf(std::vector<double> const & x, std::vector<double> const & old_y, std::vector<double> const & new_y, int const & interpolation_scheme){
+    std::vector<double> new_x;
+    new_x.resize(new_y.size());
+    for (int i=0; i<new_y.size(); i++){
+        int insert = std::lower_bound(old_y.begin()+1, old_y.end(), new_y[i]) - old_y.begin();
+        new_x[i] = ENDF_interp(old_y[insert-1], old_y[insert], x[insert-1], x[insert], new_y[i], interpolation_scheme);
+    }
+    return new_x;
 }

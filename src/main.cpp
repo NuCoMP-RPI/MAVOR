500982229c8ca4c918fa2de927492d001f4b66b1
Camden Blake <blakec4@rpi.edu>
2024-05-02 12:02:36 -0400

    Add more options

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,14 @@ int main(int argc, char* argv[]){
     argv = mavor.ensure_utf8(argv);
     mavor.add_option("-i,--input_file", input_file, "Sets the file name (and path) to be processed.");
     mavor.add_option("-o,--output_file", output_file, "Sets the file name (and path) to store the sampling distributions.");
+    mavor.add_option("--beta_integration", beta_integration_scheme, "Controls how the integration in the beta direction is handled");
+    mavor.add_option("--beta_interpolation", beta_interpolation_scheme, "Controls how the interpolation in the beta direction is handled");
+    mavor.add_option("--alpha_integration", alpha_integration_scheme, "Controls how the integration in the alpha direction is handled");
+    mavor.add_option("--alpha_interpolation", alpha_interpolation_scheme, "Controls how the interpolation in the alpha direction is handled");
+    mavor.add_option("--beta_cdf_points", num_beta_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
+    mavor.add_option("--beta_cdf_extent", beta_cdf_extent, "Sets the extent of the logistic regression function used to generate the beta CDF points.  The higher the value, the denser the points close to 0 and 1");
+    mavor.add_option("--alpha_cdf_points", num_alpha_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
+    mavor.add_option("--alpha_cdf_extent", alpha_cdf_extent, "Sets the extent fo the logistic regression function used to generate the alpha CDF points.  The higher the value, the denser the points close to 0 and 1");
     CLI11_PARSE(mavor, argc, argv);
 
     // Process the file    

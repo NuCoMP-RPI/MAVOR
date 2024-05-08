b93a309908a3be5201839475be8640e5f0f5b278
Camden Blake <blakec4@rpi.edu>
2024-05-08 12:23:39 -0400

    Move predefined energies into own files

---
+++ include/predefined_energy_grids.hpp
@@ -0,0 +1,9 @@
+#ifndef MAVOR_PREDEFINED_ENERGY_GRIDS_H
+#define MAVOR_PREDEFINED_ENERGY_GRIDS_H
+
+#include <map>
+#include <vector>
+
+extern std::map<int, std::vector<double>> predefined_energy_grids;
+
+#endif

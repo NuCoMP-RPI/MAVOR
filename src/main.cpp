48850918c39d3cbec05d7bd2c6cc00655b901311
Camden Blake <blakec4@rpi.edu>
2024-05-09 15:06:38 -0400

    Initial Setup

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <CLI/CLI.hpp>
 #include <chrono>
 
-#include "process_file.hpp"
+// #include "process_file.hpp"
 #include "runtime_variables.hpp"
 
 void print_name(){
@@ -22,33 +22,49 @@ _________\///______________\///__\///________\///________\///_____________\/////
 
 int main(int argc, char* argv[]){
     auto main_process_start = std::chrono::high_resolution_clock::now();
-    // Parse the command line arguments
-    CLI::App mavor{"Generates S(a,b) TSL sampling distributions."};
+
+    // Set up main options
+    CLI::App mavor{"Your place to make incoherent inelastic TSL sampling distributions."};
     argv = mavor.ensure_utf8(argv);
-    mavor.add_option("-i,--input_file", input_file, "Sets the file name (and path) to be processed.");
-    mavor.add_option("-o,--output_file", output_file, "Sets the file name (and path) to store the sampling distributions.");
-    mavor.add_option("--beta_integration", beta_integration_scheme, "Controls how the integration in the beta direction is handled");
-    mavor.add_option("--beta_interpolation", beta_interpolation_scheme, "Controls how the interpolation in the beta direction is handled");
-    mavor.add_option("--alpha_integration", alpha_integration_scheme, "Controls how the integration in the alpha direction is handled");
-    mavor.add_option("--alpha_interpolation", alpha_interpolation_scheme, "Controls how the interpolation in the alpha direction is handled");
-    mavor.add_option("--beta_cdf_points", num_beta_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
-    mavor.add_option("--beta_cdf_extent", beta_cdf_extent, "Sets the extent of the logistic regression function used to generate the beta CDF points.  The higher the value, the denser the points close to 0 and 1");
-    mavor.add_option("--alpha_cdf_points", num_alpha_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
-    mavor.add_option("--alpha_cdf_extent", alpha_cdf_extent, "Sets the extent fo the logistic regression function used to generate the alpha CDF points.  The higher the value, the denser the points close to 0 and 1");
-    mavor.add_option("--beta_max", beta_max, "Sets the maximum beta value to consider.");    
-    mavor.add_option("--sct_cutoff", sct_cutoff, "Sets the cutoff for the transition to the Short Collision Time Approximation.");
-    mavor.add_option("--alpha_min", alpha_min, "Sets the minimum integration alpha value");
-    mavor.add_option("--e_min", e_min, "Sets the minimum incident energy to consider.");
-    mavor.add_option("--num_energies", num_energies, "Sets the number of energies that are used to initialize the incident energy grid.");
+    mavor.set_help_all_flag("-H,--help_all", "Expands all help messages");
     mavor.add_flag("-s,--silent", silence, "Disables the terminal output");
     mavor.add_flag("-v,--verbose", verbose, "Enables additional terminal output (NOT SUPPORTED YET)");
     mavor.add_flag("-l,--log", log_run, "Enables logging (NOT SUPPORTED YET)");
     mavor.add_flag("-n,--no_process", no_process, "Disables the processing of the data.  Allows for immediate program termination.");
 
-    auto *energy = mavor.add_option_group("Incident Energy Settings", "Sets how the incident energy grid is determined");
-    auto user_energy_option = energy->add_option("-u,--user_energy_grid", energy_grid_loc, "Sets the location for a user defined energy grid to be used");
-    auto predefined_energy_option = energy->add_flag("-p,--predefined_energy_grid", use_internal_energy_grid, "Tells the program to use the predefined energy grid");
-    energy->require_option(0,1);
+    // NJOY subcommand
+    CLI::App &njoy = *mavor.add_subcommand("njoy", "Runs NJOY and LEAPR to generate TSL data.");
+    njoy.ignore_case();
+
+    // SAB subcommand
+    CLI::App &sab = *mavor.add_subcommand("sab", "Generates TSL sampling data given the output from LEAPR or similar tools.");
+    sab.ignore_case();
+    sab.add_option("-i,--input_file", input_file, "Sets the file name (and path) to be processed.");
+    sab.add_option("-o,--output_file", output_file, "Sets the file name (and path) to store the sampling distributions.");
+    sab.add_option("--beta_integration", beta_integration_scheme, "Controls how the integration in the beta direction is handled");
+    sab.add_option("--beta_interpolation", beta_interpolation_scheme, "Controls how the interpolation in the beta direction is handled");
+    sab.add_option("--alpha_integration", alpha_integration_scheme, "Controls how the integration in the alpha direction is handled");
+    sab.add_option("--alpha_interpolation", alpha_interpolation_scheme, "Controls how the interpolation in the alpha direction is handled");
+    sab.add_option("--beta_cdf_points", num_beta_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
+    sab.add_option("--beta_cdf_extent", beta_cdf_extent, "Sets the extent of the logistic regression function used to generate the beta CDF points.  The higher the value, the denser the points close to 0 and 1");
+    sab.add_option("--alpha_cdf_points", num_alpha_cdf_points, "Sets the number of CDF points to be used in fitting the beta sampling distributions");
+    sab.add_option("--alpha_cdf_extent", alpha_cdf_extent, "Sets the extent fo the logistic regression function used to generate the alpha CDF points.  The higher the value, the denser the points close to 0 and 1");
+    sab.add_option("--beta_max", beta_max, "Sets the maximum beta value to consider.");    
+    sab.add_option("--sct_cutoff", sct_cutoff, "Sets the cutoff for the transition to the Short Collision Time Approximation.");
+    sab.add_option("--alpha_min", alpha_min, "Sets the minimum integration alpha value");
+    sab.add_option("--e_min", e_min, "Sets the minimum incident energy to consider.");
+    sab.add_option("--num_energies", num_energies, "Sets the number of energies that are used to initialize the incident energy grid.");
+    auto &energy = *sab.add_option_group("Incident Energy Settings", "Sets how the incident energy grid is determined");
+    auto user_energy_option = energy.add_option("-u,--user_energy_grid", energy_grid_loc, "Sets the location for a user defined energy grid to be used");
+    auto predefined_energy_option = energy.add_flag("-p,--predefined_energy_grid", use_internal_energy_grid, "Tells the program to use the predefined energy grid");
+    energy.require_option(0,1);
+
+    // OTF subcommand
+    CLI::App &otf = *mavor.add_subcommand("otf", "Generates the OTF sampling coefficients given unionized Sab TSL sampling distributions");
+    otf.ignore_case();
+
+    // Require that at most one subcommand is passed
+    mavor.require_subcommand(0, 1);
 
     CLI11_PARSE(mavor, argc, argv);
 
@@ -59,14 +75,14 @@ int main(int argc, char* argv[]){
         print_name();
     }
 
-    // Process the file    
-    if (!no_process){
-        process_file(input_file, output_file);
-    }
+    // // Process the file    
+    // if (!no_process){
+    //     process_file(input_file, output_file);
+    // }
     auto main_process_end = std::chrono::high_resolution_clock::now();
     auto main_process_duration = std::chrono::duration_cast<std::chrono::milliseconds>(main_process_end-main_process_start);
-    if (!silence){
+    // if (!silence){
         std::cout << "Time to process the file | milliseconds " << main_process_duration.count() << std::endl;
-    }
+    // }
     return 0;
 }

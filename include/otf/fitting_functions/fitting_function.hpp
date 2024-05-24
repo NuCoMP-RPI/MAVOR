fee67fa8daabc4f26c5d9596ff92d03d88923e59
Camden Blake <blakec4@rpi.edu>
2024-05-24 14:13:55 -0400

    create fit function variables

--- include/otf/fitting_functions/fitting_function.hpp
+++ include/otf/fitting_functions/fitting_function.hpp
@@ -7,6 +7,22 @@
 typedef double(*FuncPointer)(const double &, const int);
 typedef std::pair<std::string, FuncPointer> FitFunction;
 
+extern FitFunction chebyshev;
+extern FitFunction cosine;
+extern FitFunction exponential;
+extern FitFunction inverse_exponential;
+extern FitFunction inverse_log_power;
+extern FitFunction inverse_power;
+extern FitFunction inverse_sqrt_log_power;
+extern FitFunction inverse_sqrt_power;
+extern FitFunction legendre;
+extern FitFunction log_power;
+extern FitFunction power;
+extern FitFunction sine_cosine;
+extern FitFunction sine;
+extern FitFunction sqrt_log_power;
+extern FitFunction sqrt_power;
+
 extern std::map<int, FitFunction> fitting_functions;
 
 #endif
--- src/otf/fitting_functions/fitting_function.cpp
+++ src/otf/fitting_functions/fitting_function.cpp
@@ -16,20 +16,36 @@
 #include "sqrt_log_power.hpp"
 #include "sqrt_power.hpp"
 
+FitFunction chebyshev = {"Chebyshev", eval_chebyshev_order};
+FitFunction cosine = {"cosine", eval_cosine_order};
+FitFunction exponential = {"exponential", eval_exponential_order};
+FitFunction inverse_exponential = {"inverse_exponential", eval_inverse_exponential_order};
+FitFunction inverse_log_power = {"inverse_log_power", eval_inverse_log_power_order};
+FitFunction inverse_power = {"inverse_power", eval_inverse_power_order};
+FitFunction inverse_sqrt_log_power = {"inverse_sqrt_log_power", eval_inverse_sqrt_log_power_order};
+FitFunction inverse_sqrt_power = {"inverse_sqrt_power", eval_inverse_sqrt_power_order};
+FitFunction legendre = {"legendre", eval_legendre_order};
+FitFunction log_power = {"log_power", eval_log_power_order};
+FitFunction power = {"power", eval_power_order};
+FitFunction sine_cosine = {"sine_cosine", eval_sine_cosine_order};
+FitFunction sine = {"sine", eval_sine_order};
+FitFunction sqrt_log_power = {"sqrt_log_power", eval_sqrt_log_power_order};
+FitFunction sqrt_power = {"sqrt_power", eval_sqrt_power_order};
+
 std::map<int, FitFunction> fitting_functions = {
-    {0, {"Chebyshev", eval_chebyshev_order}},
-    {1, {"cosine", eval_cosine_order}},
-    {2, {"exponential", eval_exponential_order}},
-    {3, {"inverse_exponential", eval_inverse_exponential_order}},
-    {4, {"inverse_log_power", eval_inverse_log_power_order}},
-    {5, {"inverse_power", eval_inverse_power_order}},
-    {6, {"inverse_sqrt_log_power", eval_inverse_sqrt_log_power_order}},
-    {7, {"inverse_sqrt_power", eval_inverse_sqrt_power_order}},
-    {8, {"legendre", eval_legendre_order}},
-    {9, {"log_power", eval_log_power_order}},
-    {10, {"power", eval_power_order}},
-    {11, {"sine_cosine", eval_sine_cosine_order}},
-    {12, {"sine", eval_sine_order}},
-    {13, {"sqrt_log_power", eval_sqrt_log_power_order}},
-    {14, {"sqrt_power", eval_sqrt_power_order}},
+    {0, chebyshev},
+    {1, cosine},
+    {2, exponential},
+    {3, inverse_exponential},
+    {4, inverse_log_power},
+    {5, inverse_power},
+    {6, inverse_sqrt_log_power},
+    {7, inverse_sqrt_power},
+    {8, legendre},
+    {9, log_power},
+    {10, power},
+    {11, sine_cosine},
+    {12, sine},
+    {13, sqrt_log_power},
+    {14, sqrt_power},
 };

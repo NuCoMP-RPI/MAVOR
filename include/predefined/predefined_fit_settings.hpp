ca5cee6dd12f1d6eac533b7dfbfc15ef783677a4
Camden Blake <blakec4@rpi.edu>
2024-06-27 14:23:12 -0400

    clean up commented sections

--- include/predefined/predefined_fit_settings.hpp
+++ include/predefined/predefined_fit_settings.hpp
@@ -5,25 +5,6 @@
 #include <vector>
 #include <iostream>
 
-// typedef double(*FuncPointer)(const double &, const int);
-// typedef std::pair<std::string, FuncPointer> FitFunction;
-// typedef std::tuple<bool, double, double, FitFunction> FitSettings;
-// typedef std::pair<std::string, FitSettings> PredefinedFitSettings;
-
-// typedef std::pair<PredefinedFitSettings, int> PredefinedFit;
-// typedef std::tuple<PredefinedFit, PredefinedFit, PredefinedFit> MatPredefinedFits;
-
-// typedef std::map<int, FitFunction> MapFittingFunctions;
-// extern MapFittingFunctions fitting_functions;
-
-// typedef std::map<int, PredefinedFitSettings> PredefinedFitSettingsMap;
-// extern PredefinedFitSettingsMap predefined_fit_settings;
-
-// typedef std::map<int, MatPredefinedFits> MatPredefinedFitMap;
-// extern MatPredefinedFitMap material_predefined_fits;
-
-// extern PredefinedFit default_fit;
-
 typedef double(*FuncPointer)(const double &, const int);
 typedef std::pair<std::string, FuncPointer> BasisFunction;
 typedef std::map<int, BasisFunction> BasisFunctionMap;
@@ -49,25 +30,3 @@ extern PredefinedMaterialFitsMap optimal_material_fits;
 extern PredefinedFitGroups predefined_material_fits_groups;
 
 #endif
-
-// 27 BeinBeO
-// 44 CinSiC
-// 30 crystalline_graphite
-// 11 DinD2O
-// 37 HinCH2
-// 1  HinH2O
-// 10 HinIcelh
-// 5  HinYH2
-// 7  HinZrH
-// 71 NinUN
-// 46 OinBeO
-// 51 OinD2O
-// 50 OinIcelh
-// 75 OinUO2
-// 31 reactor_graphite_10P
-// 32 reactor_graphite_30P
-// 43 SiinSiC
-// 72 UinUN
-// 48 UinUO2
-// 55 YinYH2
-// 58 ZrinZrH

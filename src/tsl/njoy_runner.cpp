f7e3749b6f0005248864715017587942bfb61c2e
Camden Blake <blakec4@rpi.edu>
2024-10-10 15:21:35 -0400

    parallel njoy run

--- src/tsl/njoy_runner.cpp
+++ src/tsl/njoy_runner.cpp
@@ -11,15 +11,12 @@ void execute_njoy(const std::filesystem::path &file, const int sim_num){
     sim_loc = sim_loc / std::to_string(sim_num);
     sim_loc = std::filesystem::absolute(sim_loc);
     std::filesystem::create_directory(sim_loc);
-    std::filesystem::path njoy_exe(tsl_njoy_exe_loc);
-    njoy_exe = std::filesystem::absolute(njoy_exe);
-    std::filesystem::copy_options copy_set = std::filesystem::copy_options::overwrite_existing;
-    std::filesystem::copy_file(njoy_exe, (sim_loc / "njoy"), copy_set);
-    std::filesystem::copy_file(file, (sim_loc / "input"), copy_set);
-    std::filesystem::path orig_work_dir = std::filesystem::current_path();
-    std::filesystem::current_path(sim_loc);
-    int _ = system("./njoy < input > out");
-    std::filesystem::current_path(orig_work_dir);
+    std::filesystem::path njoy_exe_src(tsl_njoy_exe_loc);
+    njoy_exe_src = std::filesystem::absolute(njoy_exe_src);
+    std::filesystem::copy_file(njoy_exe_src, sim_loc / "njoy");
+    std::filesystem::copy_file(file, sim_loc / "input");
+    std::string command = "cd " + sim_loc.string() + " && ./njoy < input > out";
+    int _ = system(command.c_str());
 }
 
 void njoy_runner(){
@@ -33,8 +30,10 @@ void njoy_runner(){
     } catch (const std::filesystem::filesystem_error& err) {
         std::cerr << "Error: " << err.what() << std::endl;
     }
-
+    if (std::filesystem::exists(tsl_njoy_sim_loc)){std::filesystem::remove_all(tsl_njoy_sim_loc);}
     std::filesystem::create_directory(tsl_njoy_sim_loc);
+
+    #pragma omp parallel for
     for (int i = 0; i < file_paths.size(); ++i) {
         execute_njoy(file_paths[i], i);
     }
